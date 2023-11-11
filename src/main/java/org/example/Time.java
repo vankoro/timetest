16f894576529bcdf7cbbc221943e4b295162403f
vankoro <ki-2004@yandex.ru>
2023-11-11 15:17:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Time.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.time.LocalTime;
+
+public class Time {
+
+    private LocalTime time;
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+}
+
+
